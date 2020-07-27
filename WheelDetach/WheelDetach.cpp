d129d522c62e0087fff529f799f8e9e27baa780a
Valdir da Costa Júnior <junior_djjr@live.com>
2020-07-27 05:19:28 -0300

    v1.1

--- WheelDetach/WheelDetach.cpp
+++ WheelDetach/WheelDetach.cpp
@@ -1,11 +1,30 @@
 #include "plugin.h"
 #include "IniReader\IniReader.h"
+#include "CTimer.h"
+#include "CGeneral.h"
 
 using namespace plugin;
 
+float randomChanceToBurst = 10;
 float minIntensity = 300.0f;
 float maxHealth = 700.0f;
 float radius = 1.3f;
+float minFallSpeed = 0.5f;
+float suspCompressBurst = 0.1f;
+float suspCompressDetach = 0.01f;
+
+class ExtendedData {
+public:
+	bool isFalling;
+	unsigned int lastTimeFalling;
+
+	ExtendedData(CVehicle *vehicle)
+	{
+		isFalling = false;
+		lastTimeFalling = 0;
+	}
+};
+VehicleExtendedData<ExtendedData> vehExData;
 
 void HideAllAtomics(RwFrame *frame)
 {
@@ -27,6 +46,15 @@ void HideAllAtomics(RwFrame *frame)
 	return;
 }
 
+RwFrame * HideAllNodesRecursive(RwFrame *frame)
+{
+	HideAllAtomics(frame);
+
+	if (RwFrame * nextFrame = frame->child) HideAllNodesRecursive(nextFrame);
+	if (RwFrame * nextFrame = frame->next)  HideAllNodesRecursive(nextFrame);
+	return frame;
+}
+
 bool IsNotLastWheel(CAutomobile *automobile, int wheelId)
 {
 	for (int i = 0; i < 4; ++i)
@@ -37,6 +65,23 @@ bool IsNotLastWheel(CAutomobile *automobile, int wheelId)
 		}
 	}
 	return false;
+}
+
+void DetachThisWheel(CVehicle *vehicle, int wheelNode, int wheelId, bool isRear)
+{
+	CAutomobile *automobile = reinterpret_cast<CAutomobile*>(vehicle);
+	if (vehicle->IsComponentPresent(wheelNode) && IsNotLastWheel(automobile, wheelId))
+	{
+		if (wheelId != -1)
+		{
+			if (!isRear || !vehicle->m_pHandlingData->m_nModelFlags.m_bDoubleRwheels)
+			{
+				automobile->m_damageManager.SetWheelStatus(wheelId, 2);
+			}
+		}
+		automobile->SpawnFlyingComponent(wheelNode, 1);
+		HideAllAtomics(automobile->m_aCarNodes[wheelNode]);
+	}
 }
 
 class WheelDetach
@@ -50,61 +95,138 @@ public:
 			minIntensity = ini.ReadFloat("Limits", "MinIntensity", minIntensity);
 			maxHealth = ini.ReadFloat("Limits", "MaxHealth", maxHealth);
 			radius = ini.ReadFloat("Limits", "Radius", radius);
+			randomChanceToBurst = ini.ReadInteger("Limits", "RandomChanceToBurst", randomChanceToBurst);
+			minFallSpeed = ini.ReadFloat("Limits", "MinFallSpeed", minFallSpeed);
+			suspCompressBurst = ini.ReadFloat("Limits", "SuspCompressBurst", suspCompressBurst);
+			suspCompressDetach = ini.ReadFloat("Limits", "SuspCompressDetach", suspCompressDetach);
 		}
+		minFallSpeed *= -1.0f;
 
 		Events::vehicleRenderEvent += [](CVehicle *vehicle)
 		{
-			if (vehicle && vehicle->m_fHealth < maxHealth && vehicle->m_fDamageIntensity > 1.0f &&
-				(vehicle->m_nVehicleSubClass == VEHICLE_AUTOMOBILE || vehicle->m_nVehicleSubClass == VEHICLE_QUAD) && vehicle->m_nModelIndex != MODEL_RHINO)
+			if (vehicle && (vehicle->m_nVehicleSubClass == VEHICLE_AUTOMOBILE || vehicle->m_nVehicleSubClass == VEHICLE_QUAD) &&
+				vehicle->m_nModelIndex != MODEL_RHINO)
 			{
-				float minIntensityNow = minIntensity * (vehicle->m_fHealth / maxHealth) / vehicle->m_pHandlingData->m_fCollisionDamageMultiplier;
-				if (vehicle->m_fDamageIntensity > minIntensityNow)
+				CAutomobile *automobile = reinterpret_cast<CAutomobile*>(vehicle);
+				ExtendedData &xdata = vehExData.Get(vehicle);
+				int numContactWheels = vehicle->GetNumContactWheels();
+
+				if (numContactWheels == 0 && vehicle->m_vecMoveSpeed.z < minFallSpeed)
 				{
-					CAutomobile *automobile = reinterpret_cast<CAutomobile*>(vehicle);
-					CVector collisionPos = vehicle->m_vecLastCollisionPosn;
-
-					int wheelNode = 2;
-					
-					while (true)
+					xdata.lastTimeFalling = CTimer::m_snTimeInMilliseconds;
+					xdata.isFalling = true;
+				}
+				else
+				{
+					if (xdata.isFalling)
 					{
-						if (automobile->m_aCarNodes[wheelNode] && DistanceBetweenPoints(automobile->m_aCarNodes[wheelNode]->ltm.pos, collisionPos) < radius) break;
-						wheelNode++;
-						if (wheelNode > 7) return;
-					}
-
-					int wheelId = -1;
-					bool isRear = false;
-					switch (wheelNode)
-					{
-					case eCarNodes::CAR_WHEEL_LF: //5
-						wheelId = 0;
-						break;
-					case eCarNodes::CAR_WHEEL_LB: //7
-						wheelId = 1;
-						isRear = true;
-						break;
-					case eCarNodes::CAR_WHEEL_RF: //2
-						wheelId = 2;
-						break;
-					case eCarNodes::CAR_WHEEL_RB: //4
-						wheelId = 3;
-						isRear = true;
-						break;
-					default:
-						break;
-					}
-
-					if (vehicle->IsComponentPresent(wheelNode) && IsNotLastWheel(automobile, wheelId))
-					{
-						if (wheelId != -1)
+						for (int wheelId = 0; wheelId < 4; ++wheelId)
 						{
-							if (!isRear || !vehicle->m_pHandlingData->m_nModelFlags.m_bDoubleRwheels)
+							float suspAmount = reinterpret_cast<CAutomobile*>(vehicle)->wheelsDistancesToGround2[wheelId];
+							if (suspAmount < suspCompressBurst)
 							{
-								automobile->m_damageManager.SetWheelStatus(wheelId, 2);
+								if (suspAmount < suspCompressDetach)
+								{
+									int wheelNode;
+									bool isRear = false;
+									switch (wheelId)
+									{
+									case 0:
+										wheelNode = eCarNodes::CAR_WHEEL_LF;
+										break;
+									case 1:
+										wheelNode = eCarNodes::CAR_WHEEL_LB;
+										isRear = true;
+										break;
+									case 2:
+										wheelNode = eCarNodes::CAR_WHEEL_RF;
+										break;
+									case 3:
+										wheelNode = eCarNodes::CAR_WHEEL_RB;
+										isRear = true;
+										break;
+									default:
+										break;
+									}
+									DetachThisWheel(vehicle, wheelNode, wheelId, isRear);
+								}
+								else
+								{
+									vehicle->BurstTyre(wheelId, true);
+								}
 							}
 						}
-						automobile->SpawnFlyingComponent(wheelNode, 1);
-						HideAllAtomics(automobile->m_aCarNodes[wheelNode]);
+					}
+					// Reset falling if touched the ground after time limit
+					if (xdata.isFalling && (CTimer::m_snTimeInMilliseconds - xdata.lastTimeFalling) > 50)
+					{
+						xdata.isFalling = false;
+						xdata.lastTimeFalling = 0;
+					}
+				}
+
+				if (vehicle->m_fHealth < maxHealth)
+				{
+					float minIntensityNow = minIntensity * (vehicle->m_fHealth / maxHealth) / vehicle->m_pHandlingData->m_fCollisionDamageMultiplier;
+					if (vehicle->m_fDamageIntensity > minIntensityNow)
+					{
+						CVector collisionPos = vehicle->m_vecLastCollisionPosn;
+
+						int wheelNode = 2;
+
+						while (true)
+						{
+							if (automobile->m_aCarNodes[wheelNode] && abs(DistanceBetweenPoints(automobile->m_aCarNodes[wheelNode]->ltm.pos, collisionPos)) < radius) break;
+							wheelNode++;
+							if (wheelNode > 7) return;
+						}
+
+						int wheelId = -1;
+						bool isRear = false;
+						switch (wheelNode)
+						{
+						case eCarNodes::CAR_WHEEL_LF: //5
+							wheelId = 0;
+							break;
+						case eCarNodes::CAR_WHEEL_LB: //7
+							wheelId = 1;
+							isRear = true;
+							break;
+						case eCarNodes::CAR_WHEEL_RF: //2
+							wheelId = 2;
+							break;
+						case eCarNodes::CAR_WHEEL_RB: //4
+							wheelId = 3;
+							isRear = true;
+							break;
+						default:
+							break;
+						}
+
+						if (vehicle->IsComponentPresent(wheelNode) && automobile->m_damageManager.GetWheelStatus(wheelId) != 2 && IsNotLastWheel(automobile, wheelId))
+						{
+							if (wheelId != -1)
+							{
+								if (CGeneral::GetRandomNumberInRange(0, 100) < randomChanceToBurst)
+								{
+									vehicle->BurstTyre(wheelId, true);
+									return;
+								}
+								else
+								{
+									if (!isRear || !vehicle->m_pHandlingData->m_nModelFlags.m_bDoubleRwheels)
+									{
+										automobile->m_damageManager.SetWheelStatus(wheelId, 2);
+									}
+								}
+							}
+							automobile->SpawnFlyingComponent(wheelNode, 1);
+							HideAllAtomics(automobile->m_aCarNodes[wheelNode]);
+							if (automobile->m_aCarNodes[wheelNode]->child)
+							{
+								HideAllNodesRecursive(automobile->m_aCarNodes[wheelNode]->child);
+							}
+						}
 					}
 				}
 			}
